efc6189a8889aef5b0ed5d667a481e0075b2d9cb
SuhailSherief <suhailscreed@gmail.com>
2023-01-02 22:47:12 +0530

    componentizing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>All Todo's</h1>
-      <div className="card">
-        <h2>Title</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo title="medicine"/>
+      <Todo title="food"/>
+      <Todo title="gym"/>
     </div>
   );
 }

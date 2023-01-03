3054cda9241773c1e49474df301a2bb150d4e4b0
SuhailSherief <suhailscreed@gmail.com>
2023-01-03 23:22:43 +0530

    added backdrop to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Backdrop from "./components/Backdrop";
-import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
       <Todo title="medicine"/>
       <Todo title="food"/>
       <Todo title="gym"/>
-      <Modal />
-      <Backdrop />
     </div>
   );
 }

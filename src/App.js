7009d8c556f275b1c6ecbc694a97ee88eae5af11
Suhail Sherief <suhailsherief@Suhails-MacBook-Pro.local>
2023-01-09 23:46:12 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" exact element={<AllMeetups />} />
+        <Route path="/new-meetup" exact element={<NewMeetup />} />
+        <Route path="/favorites" exact element={<Favorites />} />
+      </Routes>
     </div>
   );
 }

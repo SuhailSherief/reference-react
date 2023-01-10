1d7af6c8a6045268b10267adf74d09c5cb329697
SuhailSherief <suhailscreed@gmail.com>
2023-01-11 00:00:43 +0530

    navbar changes

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import classes from './MainNavigation.module.css'
 
 function MainNavigation() {
   return (
-    <header>
-        <div>React Meetup</div>
+    <header className={classes.header}>
+        <div className={classes.logo}>React Meetup</div>
         <nav>
             <ul>
                 <li>
-                    <Link to='/'>All Meetups</Link>
+                    <Link to='/'>All Meetup</Link>
                 </li>
                 <li>
                     <Link to='/new-meetup'>Add Meetup</Link>
                 </li>
                 <li>
-                    <Link to='/favorites'>Favorite Meetups</Link>
+                    <Link to='/favorites'>Favorite Meetup</Link>
                 </li>
             </ul>
         </nav>

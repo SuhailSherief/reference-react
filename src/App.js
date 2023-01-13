8aa756e005051b3a96d8428643009f0fc5dd69e7
SuhailSherief <suhailscreed@gmail.com>
2023-01-13 23:57:54 +0530

    added layout

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" exact element={<AllMeetups />} />
-        <Route path="/new-meetup" exact element={<NewMeetup />} />
-        <Route path="/favorites" exact element={<Favorites />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" exact element={<AllMeetups />} />
+          <Route path="/new-meetup" exact element={<NewMeetup />} />
+          <Route path="/favorites" exact element={<Favorites />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
